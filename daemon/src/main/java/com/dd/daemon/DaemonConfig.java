bbed0833704fcf3f451d56a474032dd8fb24b066
Binh Tran Ngoc Thanh <tranngocthanhbinh01@gmail.com>
2026-03-11 11:00:24 +0700

    Implement the main daemin process

--- daemon/src/main/java/com/dd/daemon/DaemonConfig.java
+++ daemon/src/main/java/com/dd/daemon/DaemonConfig.java
@@ -5,11 +5,12 @@ public record DaemonConfig(
         String host,
         int port,
         int peerPort,
+        String directoryHost,
         int directoryPort,
         long heartbeatInterval,
         long shareDir) {
     public static DaemonConfig fromArgs(String[] args) {
-        if (args.length != 7) {
+        if (args.length != 8) {
             throw new IllegalArgumentException(
                     "Usage: <clientId> <host> <peerPort> <sharedDir> [directoryHost=localhost] [directoryPort=1099] [heartbeatMs=5000]");
         }
@@ -18,8 +19,9 @@ public record DaemonConfig(
                 args[1],
                 Integer.parseInt(args[2]),
                 Integer.parseInt(args[3]),
-                Integer.parseInt(args[4]),
-                Long.parseLong(args[5]),
-                Long.parseLong(args[6]));
+                args[4],
+                Integer.parseInt(args[5]),
+                Long.parseLong(args[6]),
+                Long.parseLong(args[7]));
     }
 }
---
+++ daemon/src/main/java/com/dd/daemon/DaemonMain.java
@@ -0,0 +1,32 @@
+package com.dd.daemon;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class DaemonMain {
+    public static void main(String[] args) {
+        DaemonConfig config = DaemonConfig.fromArgs(args);
+        DirectoryClient directoryClient = new DirectoryClient(config.directoryHost(), config.directoryPort());
+
+        // Register client
+        directoryClient.registerClient(config.clientId(), config.host(), config.peerPort());
+        System.out.println("Registered client " + config.clientId() + " with directory service.");
+
+        // Scan and register files
+        LocalFileCatalog catalog = new LocalFileCatalog(config.sharedDirectory());
+        Map<String, Path> files = catalog.scan();
+
+        for (Path p : files.values()) {
+            long size = Files.size(p);
+            directoryClient.registerFile(config.clientId(), p, size);
+            System.out
+                    .println("Registered file " + p.getFileName() + " with size " + size + " with directory service.");
+        }
+
+        HeartbeatWorker heartbeatWorker = new HeartbeatWorker();
+        heartbeatWorker.start(directoryClient, config.clientId(), config.heartbeatInterval());
+
+        Thread.currentThread().join();
+    }
+}
